f459a62701287301faefac52cc11a6e378e86f0b
Arthur <asad.artur@gmail.com>
2020-10-13 22:01:37 +0300

    методы представления заменены на классы

--- testsite/news/forms.py
+++ testsite/news/forms.py
@@ -1,8 +1,21 @@
 from django import forms
-from . models import Category
+from . models import News
+import re
+from django.core.exceptions import ValidationError
 
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label='Название')
-    content = forms.CharField(label='Текст', required=False)
-    is_published = forms.BooleanField(label='Опубликовано', initial=True)
-    category = forms.ModelChoiceField(empty_label = 'Выберите категорию',queryset=Category.objects.all(), label='Категория')
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        #fields = '__all__'
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'form-control'}),
+            'content':forms.TextInput(attrs={'class':'form-control', 'rows': 5}),
+            "category":forms.Select(attrs={'class':'form-control'})
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title
